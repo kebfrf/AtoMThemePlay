0c0bd0ac561fbc5da0ac9137c596154e6e432a8d
kebfrf <jjay.keeffe@gmail.com>
2017-03-12 15:37:27 +1100

    Update _header.php

--- templates/_header.php
+++ templates/_header.php
@@ -1,47 +1,78 @@
-<?php echo get_component_slot('header') ?>
-
+Skip to content
+This repository
+Search
+Pull requests
+Issues
+Gist
+ @kebfrf
+ Sign out
+ Watch 30
+  Star 63
+  Fork 51 artefactual/atom
+ Code  Issues 0  Pull requests 3  Projects 0  Pulse  Graphs
+Tree: c1a656e04c Find file Copy pathatom/apps/qubit/templates/_header.php
+c1a656e  15 days ago
+@mcantelon mcantelon Warning translation and CSS fixes, refs #10473
+3 contributors @sevein @jraddaoui @mcantelon
+RawBlameHistory     
+60 lines (41 sloc)  1.82 KB
 <?php echo get_component('default', 'updateCheck') ?>
 
-<?php if ($sf_user->isAuthenticated()): ?>
-  <div id="top-bar">
-    <nav>
-      <?php echo get_component('menu', 'userMenu') ?>
-      <?php echo get_component('menu', 'quickLinksMenu') ?>
-      <?php if (sfConfig::get('app_toggleLanguageMenu')): ?>
-        <?php echo get_component('menu', 'changeLanguageMenu') ?>
-      <?php endif; ?>
-      <?php echo get_component('menu', 'mainMenu', array('sf_cache_key' => $sf_user->getCulture().$sf_user->getUserID())) ?>
-    </nav>
+<?php if ($sf_user->isAdministrator() && (string)QubitSetting::getByName('siteBaseUrl') === ''): ?>
+  <div class="site-warning">
+    <?php echo link_to(__('Please configure your site base URL'), 'settings/siteInformation', array('rel' => 'home', 'title' => __('Home'))) ?>
   </div>
 <?php endif; ?>
 
-<div id="header">
+<header id="top-bar">
 
-  <div class="container">
-
-    <div id="header-lvl2">
-      <div class="row">
-
-        <div id="logo-and-name" class="span6">
-            <h1><?php echo link_to(image_tag('/plugins/arGraingerPlugin/images/graingerlogo.png', array('alt' => __('Grainger Museum Archive'))), 'http://10.100.148.228', array('rel' => 'home')) ?></h1>
+  <?php if (sfConfig::get('app_toggleLogo')): ?>
+    <h1><?php echo link_to(image_tag('/plugins/arGraingerPlugin/images/logo.png', array('alt' => __('Grainger Museum Archive'))), 'http://10.100.148.228', array('rel' => 'home')) ?></h1>
           
-        </div>
+  <?php endif; ?>
 
-        <div id="header-search" class="span6">
-          <?php echo get_component('search', 'box') ?>
+  <?php if (sfConfig::get('app_toggleTitle')): ?>
+    <h1 id="site-name">
+      <?php echo link_to('<span>'.esc_specialchars(sfConfig::get('app_siteTitle')).'</span>', '@homepage', array('rel' => 'home', 'title' => __('Home'))) ?>
+    </h1>
+  <?php endif; ?>
 
-          <?php echo get_component('menu', 'clipboardMenu') ?>
-        </div>
+  <nav>
 
-      </div>
-    </div>
+    <?php echo get_component('menu', 'userMenu') ?>
+
+    <?php echo get_component('menu', 'quickLinksMenu') ?>
+
+    <?php if (sfConfig::get('app_toggleLanguageMenu')): ?>
+      <?php echo get_component('menu', 'changeLanguageMenu') ?>
+    <?php endif; ?>
+
+    <?php echo get_component('menu', 'clipboardMenu') ?>
+
+    <?php echo get_component('menu', 'mainMenu', array('sf_cache_key' => $sf_user->getCulture().$sf_user->getUserID())) ?>
+
+  </nav>
+
+  <div id="search-bar">
+
+    <?php echo get_component('menu', 'browseMenu', array('sf_cache_key' => $sf_user->getCulture().$sf_user->getUserID())) ?>
+
+    <?php echo get_component('search', 'box') ?>
 
   </div>
 
-</div>
+  <?php echo get_component_slot('header') ?>
 
+</header>
 
-  
- 
-
- 
+<?php if (sfConfig::get('app_toggleDescription')): ?>
+  <div id="site-slogan">
+    <div class="container">
+      <div class="row">
+        <div class="span12">
+          <span><?php echo esc_specialchars(sfConfig::get('app_siteDescription')) ?></span>
+        </div>
+      </div>
+    </div>
+  </div>
+<?php endif; ?>
