e29fa2b3effcb41f1c176f37c3f6879f63a55e6f
kebfrf <jjay.keeffe@gmail.com>
2017-03-12 13:56:50 +1100

    Update arCorcovadoPluginConfiguration.class.php

--- config/arCorcovadoPluginConfiguration.class.php
+++ config/arCorcovadoPluginConfiguration.class.php
@@ -3,7 +3,7 @@
    class arCorcovadoPluginConfiguration extends sfPluginConfiguration
    {
      public static
-       $summary = 'Theme plugin, extension of arDominionPlugin.',
+       $summary = 'Grainger theme plugin, extension of arDominionPlugin.',
        $version = '0.0.1';
 
      public function contextLoadFactories(sfEvent $event)
